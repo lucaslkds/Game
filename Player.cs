7538af5cc769dcc421a97a3747496c33ad3012d0
lucaslkds <91992988+lucaslkds@users.noreply.github.com>
2026-04-06 19:48:34 -0300

    Delete Player.cs

--- Player.cs
+++
@@ -1,8 +0,0 @@
-class Player : Character
-{
-    public Player(int maxhp, int currenthp, int attack, int heal, string name)
-        : base(maxhp, currenthp, attack, heal, name)
-    {
-        int areaattack = 5;
-    }
-}
