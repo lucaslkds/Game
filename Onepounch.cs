ec2c0650b8bd563788f83f26f7befb35c3e3a0e1
lucaslkds <91992988+lucaslkds@users.noreply.github.com>
2026-04-06 19:48:37 -0300

    Delete Onepounch.cs

--- Onepounch.cs
+++
@@ -1,9 +0,0 @@
-public class Onepounch : Character
-{
-    int ultraattack = 999999;
-
-    public Onepounch(int maxhp, int currenthp, int attack, int heal, string name)
-        : base(maxhp, currenthp, attack, heal, name)
-    {
-    }
-}
